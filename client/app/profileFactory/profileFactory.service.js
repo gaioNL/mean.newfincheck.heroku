0158809546a5b16ba386bfe617d75c78558a5aba
¨gaioNL¨ <claudio.gaiaschi@gmail.com>
2016-09-20 05:34:46 +0200

    message

--- client/app/profileFactory/profileFactory.service.js
+++ client/app/profileFactory/profileFactory.service.js
@@ -1,15 +1,9 @@
 'use strict';
 
-var config = require('./config/environment');
-
-var uristring = config.mongo.uri + ":" + config.port + "/" ;
-
-console.log("passa profileFactory " + uristring); 
-
 angular.module('newFinCopenApp')
 //old
 //.constant("baseURL", "http://localhost:5000/")
-.constant("baseURL", uristring)
+.constant("baseURL", "https://newfincheroku.herokuapp.com/")
 .service('profileFactory', function ($resource, baseURL) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     console.log("passa profileFactory");
